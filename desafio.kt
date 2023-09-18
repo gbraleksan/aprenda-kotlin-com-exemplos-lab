7a549bc1defd2cd2dc43a905501cc5f188a88d36
Aleksander <36135557+gbraleksan@users.noreply.github.com>
2023-09-18 11:48:28 -0300

    Entrega do projeto "aprenda kotlin com exemplos lab" do curso "code Update TQI - Backend com Kotlin e Java" da DIO

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,42 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
+data class Usuario(val login: String, val senha: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, var nivel: Nivel) {
+    
+    var mensagem = when(nivel){
+	Nivel.BASICO -> "nivel basico"
+	Nivel.INTERMEDIARIO -> "nivel intermediário"
+	Nivel.AVANCADO -> "nivel avançado"
+    }
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        
+        inscritos.add(usuario)
+        println("Aluno ${usuario.login} matriculado no curso de formação $nome de $mensagem!")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //Criando Conteudos
+    val javaBasic = ConteudoEducacional("Instalando a IDE do java")
+    val javaPOO = ConteudoEducacional("Programação Orientada a Objetos com Java")
+    val javaCollections = ConteudoEducacional("Introdução ao Java Collections")
+    
+    //Criando curso de formações
+    val curso1 = Formacao("\"Introdução ao Kotlin\"", listOf(javaBasic, javaPOO, javaCollections), Nivel.AVANCADO)
+    val curso2 = Formacao("\"Introdução ao Java\"", listOf(javaBasic, javaPOO, javaCollections), Nivel.BASICO)
+    
+    //criando usuários
+    val user = Usuario("Aleksander", "123")
+    val user2 = Usuario("Gbraleksan", "147")
+    
+    //Adicionando usuarios aos cursos
+    curso1.matricular(user)
+    curso2.matricular(user2)
+
 }
